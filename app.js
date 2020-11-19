e6bcbd17246e4755f2728c5c2415af0a8a22393a
kaan <darksunnn-kaan@hotmail.com>
2020-11-20 00:06:14 +0300

    Update app.js

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.get('/',function(req,res){
 fs.readFile('index.html',function(err,data){
 
     res.write(data);
-    res.write('iyi geceler :D');
+    res.write('ordada havalar soğukmu burası kutup gibi :D');
     res.end('mesaj bitti');
 
 

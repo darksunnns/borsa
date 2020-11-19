f9ebffed4be111fce5acd0d8331b8667d56e0ebb
kaan <darksunnn-kaan@hotmail.com>
2020-11-19 23:36:15 +0300

    Update app.js

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.get('/',function(req,res){
 fs.readFile('index.html',function(err,data){
 
     res.write(data);
-    res.write('kaandene me');
+    res.write('SENİN NEYİN VAR');
     res.end('mesaj bitti');
 
 

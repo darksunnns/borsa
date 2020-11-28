1d6732a7552d2c51615258f22f6f94625572beaf
kaan <darksunnn-kaan@hotmail.com>
2020-11-28 16:47:32 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,18 +1,22 @@
+const log = require ('./kaan');
+log.ceren();
+
 
 var fs = require('fs');
 
 var express = require('express');
-
+var path = require('path');
 var app = express();
 const port=process.env.PORT || 4000
 
+app.use('/public',express.static(path.join(__dirname,'public'))) 
 
 app.get('/',function(req,res){
 
 fs.readFile('index.html',function(err,data){
 
     res.write(data);
-    res.write('ordada havalar soğukmu burası kutup gibi :D');
+    res.write('kaandeneme');
     res.end('mesaj bitti');
 
 
